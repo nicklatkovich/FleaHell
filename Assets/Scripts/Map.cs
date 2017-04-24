e11e48667dc8cb2a6c31e43774d4d2d2e1b20f98
NickLatkovich <nicklatkovich@gmail.com>
2017-04-24 13:25:14 +0300

    Base movement

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject WallPrefab;
+    public uint Width, Height;
+
+    void Start( ) {
+        int[ ][ ] grid = new int[Width][ ];
+        for (uint i = 0; i < Width; i++) {
+            grid[i] = new int[Height];
+            for (uint j = 0; j < Height; j++) {
+                grid[i][j] = Random.value < 0.1f ? 1 : 0;
+            }
+        }
+
+        for (uint i = 0; i < Width; i++) {
+            uint j = Height - 1;
+            grid[i][j] = grid[i][0] = 1;
+        }
+        for (uint j = 0; j < Height; j++) {
+            uint i = Width - 1;
+            grid[i][j] = grid[0][j] = 1;
+        }
+
+        for (uint i = 0; i < Width; i++) {
+            for (uint j = 0; j < Height; j++) {
+                if (grid[i][j] == 1) {
+                    Instantiate(WallPrefab, new Vector3(i, 0.5f, j), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    void Update( ) {
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float MaxSpeed = 1f;
+    public float Dir = 0f;
+    public float Pitch = 6f;
+
+    private Transform camera;
+    private Transform spot;
+
+    void Start( ) {
+        camera = this.transform.FindChild("Main Camera");
+        spot = this.transform.FindChild("Spotlight");
+    }
+
+    void Update( ) {
+        Dir += Input.GetAxis("TargetX") * 2f * Time.deltaTime;
+        Pitch += Input.GetAxis("TargetZ") * 4f * Time.deltaTime;
+
+        Vector3 v = new Vector3(Input.GetAxis("MoveX"), 0f, Input.GetAxis("MoveZ"));
+        Vector3 norm = new Vector3(Mathf.Cos(Dir), 0f, Mathf.Sin(Dir));
+
+        //this.transform.position
+
+        this.transform.position += (new Vector3(norm.z * v.z, 0f, norm.x * v.z) + new Vector3(norm.x * v.x, 0f, -norm.z * v.x)) * MaxSpeed * Time.deltaTime;
+
+        Vector3 anti_norm = new Vector3(norm.z, 0f, norm.x);
+
+        spot.rotation = Quaternion.LookRotation(anti_norm, Vector3.up);
+
+        camera.localPosition = new Vector3(-norm.z, 0f, -norm.x) * Pitch + new Vector3(0f, camera.localPosition.y, 0f);
+
+        camera.rotation = Quaternion.LookRotation(-camera.localPosition, Vector3.up);
+    }
+}
